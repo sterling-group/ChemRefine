d63e4afaf81c1072b36ef1e61500a841e3cded65
ignaciomigliaro <ignaciomigliaro@outlook.com>
2025-05-30 15:36:33 -0500

    simplified filesubmitter to work with new orca sumbimission class.

--- chemrefine/file_submission.py
+++ chemrefine/file_submission.py
@@ -1,51 +1,24 @@
-import os
-import subprocess
-import time
-import getpass
-import re
+from .orca_interface import OrcaJobSubmitter
 import logging
-from .utils import Utility
-from .constants import JOB_CHECK_INTERVAL, DEFAULT_CORES, DEFAULT_MAX_CORES
-
 class FileSubmitter:
     def __init__(self):
-        self.utility = Utility()
+        self.orca_submitter = OrcaJobSubmitter()
 
-    def submit_qorca(self, input_file, qorca_flags=None):
-        qorca_path = os.path.join(os.path.dirname(__file__), "..", "..", "vendor", "qorca", "qorca")
-        command = ["python3", qorca_path, input_file]
-        if qorca_flags:
-            command.extend(qorca_flags)
-        result = subprocess.run(command, check=True, text=True, capture_output=True)
-        jobid_match = re.search(r'Submitted job (\d+)', result.stdout)
-        jobid = jobid_match.group(1) if jobid_match else result.stdout.strip().split()[-1]
-        pal = self.utility.extract_pal_from_qorca_output(result.stdout + "\n" + result.stderr)
-        return jobid, pal
-
-    def is_job_finished(self, job_id):
-        try:
-            username = getpass.getuser()
-            result = subprocess.run(["squeue", "-u", username, "-o", "%i", "-h"],
-                                    capture_output=True, text=True, timeout=30)
-            return job_id not in result.stdout.strip().splitlines()
-        except Exception:
-            return True
-
-    def submit_files(self, input_files, max_cores=DEFAULT_MAX_CORES, qorca_flags=None):
-        total_cores, active_jobs = 0, {}
+    def submit_files(self, input_files, max_cores, qorca_flags=None):
         for input_file in input_files:
-            cores_needed = DEFAULT_CORES
-            while total_cores + cores_needed > max_cores:
-                time.sleep(JOB_CHECK_INTERVAL)
-                finished = [jid for jid in active_jobs if self.is_job_finished(jid)]
-                for jid in finished:
-                    total_cores -= active_jobs.pop(jid)
-            job_id, pal = self.submit_qorca(input_file, qorca_flags)
-            actual_cores = pal if pal else cores_needed
-            active_jobs[job_id] = actual_cores
-            total_cores += actual_cores
-        while active_jobs:
-            time.sleep(JOB_CHECK_INTERVAL)
-            for jid in list(active_jobs):
-                if self.is_job_finished(jid):
-                    total_cores -= active_jobs.pop(jid)
+            input_path = Path(input_file)
+            
+            # 1 Read the PAL value from input
+            pal_value = self.orca_submitter.parse_pal_from_input(input_path)
+            pal_value = min(pal_value, max_cores)
+
+            # 2️ Adjust the PAL value in the input file
+            self.orca_submitter.adjust_pal_in_input(input_path, pal_value)
+
+            # 3️ Generate SLURM script
+            slurm_script = self.orca_submitter.generate_slurm_script(input_path, pal_value)
+
+            # 4️ Submit the job
+            job_id = self.orca_submitter.submit_job(slurm_script)
+            logging.info(f"Job submitted with ID: {job_id}")
+
